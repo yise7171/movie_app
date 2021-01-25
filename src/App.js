2ba0dfab3856a68ec3aedb78a1020747ff908e7a
unknown <yise7171@naver.com>
2021-01-25 18:40:27 +0900

    #1.0 React Test Movie APP 2021

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({ fav }) {
+  console.log({ fav });
+  return <h3>I Like {fav}</h3>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <h1>I love React!!!</h1>
+      <Food fav="kimchi" />
+      <Food fav="samguypsal" />
+      <Food fav="ramen" />
+      <Food fav="udong" />
+    </div>);
 }
 
 export default App;
